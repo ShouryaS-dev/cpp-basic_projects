87c00ad1398b1f97a93c173486bb77824fd23c70
ShouryaS-dev <shourya7sinha@gmail.com>
2026-06-09 21:04:38 +0530

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -19,7 +19,21 @@ int main()
     int a;
     std::cout<<"Enter the position you want to find the number in the Fibanacci series: ";
     std::cin>>a;
+
+    if (a<0)
+    {
+        std::cout<<"Fibonacci series is not defined for -ve numbers";
+        return 0;
+    }
+
+    else if (a==0)
+    {
+        std::cout<<"0";
+        return 0;
+    }
+
     std::cout<<fibonacci(a);
 
     return 0;
 }
+
